5b8837a4cef031a6e5546e90c48d281839c97d97
NoobieUs37 <NoobieUs37@protonmail.com>
2021-08-17 11:44:35 +0530

    Updates

---
+++ precache-manifest.1fb1ffb7af1f48319e94d5cf466f896d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bbafffd9ffa42602d093a617c097a61",
+    "url": "/keepernoteapp/index.html"
+  },
+  {
+    "revision": "0518db8b7925987642f2",
+    "url": "/keepernoteapp/static/js/2.d5cd440c.chunk.js"
+  },
+  {
+    "revision": "0bb6d8be0168cbbe5a01",
+    "url": "/keepernoteapp/static/js/main.b91b29d1.chunk.js"
+  },
+  {
+    "revision": "9c316dd13fd25ab61e8c",
+    "url": "/keepernoteapp/static/js/runtime-main.d5bf8673.js"
+  }
+]);
